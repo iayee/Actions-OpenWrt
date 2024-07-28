5d77e60bf2ceee2625dd9726de48245a851b9b96
iayee <48880288+iayee@users.noreply.github.com>
2024-07-28 20:45:55 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,5 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git luci-theme-argon https://github.com/jerrykuku/luci-theme-argon' >>feeds.conf.default
